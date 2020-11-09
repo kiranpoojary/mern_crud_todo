3af873821bd54da7b3d368bbc55f14ba591a7214
Kiran Poojary <kiranpoojary483@gmail.com>
2020-11-09 20:19:02 +0530

    Update connection.js

--- backend/model/connection.js
+++ backend/model/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 
-mongoose.connect("mongodb+srv://kiranpoojary:kiran123@projectcluster.rmirs.mongodb.net/todo?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+mongoose.connect("<Mongodb:application_srv>", { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
     //mongodb+srv://kiranpoojary:kiran123@projectcluster.rmirs.mongodb.net/todo?retryWrites=true&w=majority
     //mongodb://localhost:27017/Todo
     if (!err) {
